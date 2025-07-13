68d34996e80570452e694b414f9e4650115c88d1
Tarun Boddeda <121426564+TarunB-Git@users.noreply.github.com>
2025-07-14 00:18:02 +0700

    Update debt.py

--- app/models/debt.py
+++ app/models/debt.py
@@ -2,14 +2,12 @@ from app.extensions import db
 
 class Debt(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    lender_id = db.Column(db.Integer, db.ForeignKey("person.id"), nullable=False)
-    borrower_id = db.Column(db.Integer, db.ForeignKey("person.id"), nullable=False)
     amount = db.Column(db.Float, nullable=False)
+
+    debtor_id = db.Column(db.Integer, db.ForeignKey("person.id"), nullable=False)
+    creditor_id = db.Column(db.Integer, db.ForeignKey("person.id"), nullable=False)
+
+    debtor = db.relationship("Person", foreign_keys=[debtor_id], backref="debts_owed")
+    creditor = db.relationship("Person", foreign_keys=[creditor_id], backref="debts_due")
+
     is_paid = db.Column(db.Boolean, default=False)
-    lender = db.relationship("Person", foreign_keys=[lender_id], backref="lent_debts")
-    borrower = db.relationship("Person", foreign_keys=[borrower_id], backref="owed_debts")
-
-    __table_args__ = (
-        db.UniqueConstraint("lender_id", "borrower_id", name="_lender_borrower_uc"),
-    )
-
