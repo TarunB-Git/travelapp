a309418d75abcef84f903c01baa98e7fbec08fed
Tarun Boddeda <121426564+TarunB-Git@users.noreply.github.com>
2025-07-14 01:27:11 +0700

    Update person.py

--- app/models/person.py
+++ app/models/person.py
@@ -1,11 +1,5 @@
 from app.extensions import db
 
-class Group(db.Model):
-    __tablename__ = "person_group"  # Avoid reserved keyword
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True, nullable=False)
-    people = db.relationship("Person", back_populates="group")
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
