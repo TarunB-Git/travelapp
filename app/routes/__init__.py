6d61f53e19ed325ed616827875174d9a4e18f862
Tarun Boddeda <121426564+TarunB-Git@users.noreply.github.com>
2025-07-13 14:31:58 +0700

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .person_routes import person_bp
 from .budget_routes import budget_bp
 from .transaction_routes import transaction_bp
+from .export_routes import export_bp
 from .debt_routes import debt_bp
 from .views import views_bp
 from .auth_routes import auth_bp  
@@ -12,6 +13,7 @@ def register_blueprints(app: Flask) -> None:
         budget_bp,
         transaction_bp,
         debt_bp,
+export_bp,
         views_bp,
         auth_bp,  # Register auth routes so /login, /admin/login, etc. work
     ):
