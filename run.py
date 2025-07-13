87a597b008c45be9b1b6bb1e7facdd28bbb3c008
Tarun Boddeda <121426564+TarunB-Git@users.noreply.github.com>
2025-07-13 13:29:16 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,15 +1,13 @@
+
 from app import create_app
 from app.extensions import db
+from app.models.credentials import AdminCredentials
 
 app = create_app()
 
-# auto‑create tables
 with app.app_context():
-    # print all registered routes for debugging
-    for rule in app.url_map.iter_rules():
-        print(f"{rule.endpoint:30s} → {rule}")
     db.create_all()
+    AdminCredentials.init()  # ← add this line
 
 if __name__ == "__main__":
     app.run(debug=True)
-
