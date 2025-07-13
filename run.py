894467fc90b739dd4676a4544faa4359191326d4
Tarun Boddeda <121426564+TarunB-Git@users.noreply.github.com>
2025-07-14 03:40:05 +0700

    Update run.py

--- run.py
+++ run.py
@@ -6,9 +6,9 @@ from app.models.credentials import AdminCredentials
 app = create_app()
 
 with app.app_context():
-    AdminCredentials.init()
     db.create_all()
-
+    AdminCredentials.init()
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
