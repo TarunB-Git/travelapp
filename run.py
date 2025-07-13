9d31fc6efd3cb51485520f8ffac03e86c3a59974
Tarun Boddeda <121426564+TarunB-Git@users.noreply.github.com>
2025-07-14 03:42:01 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 
 from app import create_app
-from app.extensions import db
+from app.extensions import db, message_db
 from app.models.credentials import AdminCredentials
 
 app = create_app()
 
 with app.app_context():
     db.create_all()
+    message_db.create_all()
     AdminCredentials.init()
     
 
