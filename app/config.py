ba63f4effad137fd23d3214e8ec451673981e4ea
Tarun Boddeda <121426564+TarunB-Git@users.noreply.github.com>
2025-07-14 04:13:12 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,9 +1,11 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///travel_budget.db")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", "sqlite:///travel_budget.db"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "change-me")
-
-    
-    }
+    SQLALCHEMY_BINDS = {
+    "messages": "sqlite:///instance/messages.db"
+}
