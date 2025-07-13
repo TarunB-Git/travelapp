e9273d77be56b8007e823d2e44bd3d74bdb7d084
Tarun Boddeda <121426564+TarunB-Git@users.noreply.github.com>
2025-07-14 01:14:31 +0700

    Update person.py

--- app/models/person.py
+++ app/models/person.py
@@ -1,8 +1,12 @@
 from app.extensions import db
 
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    people = db.relationship("Person", back_populates="group")
+
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), unique=True, nullable=False)
+    name = db.Column(db.String(64), unique=True, nullable=False)
     group_id = db.Column(db.Integer, db.ForeignKey("group.id"))
-
-
+    group = db.relationship("Group", back_populates="people")
