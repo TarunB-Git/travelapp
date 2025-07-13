69aa9d6a1152d86ddeee4976d226ada51e54b683
Tarun Boddeda <121426564+TarunB-Git@users.noreply.github.com>
2025-07-13 14:28:31 +0700

    Update budget.py

--- app/models/budget.py
+++ app/models/budget.py
@@ -1,10 +1,15 @@
 from app.extensions import db
 
+budget_person = db.Table(
+    "budget_person",
+    db.Column("budget_id", db.Integer, db.ForeignKey("budget.id")),
+    db.Column("person_id", db.Integer, db.ForeignKey("person.id"))
+)
+
 class Budget(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     category = db.Column(db.String(50), nullable=False)
     daily_limit = db.Column(db.Float, nullable=True)
     total_limit = db.Column(db.Float, nullable=True)
-    person_id = db.Column(db.Integer, db.ForeignKey("person.id"), nullable=True)
-    person = db.relationship("Person", backref="budgets")
 
+    people = db.relationship("Person", secondary=budget_person, backref="budgets")
