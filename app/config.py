7690f0a4b2eda007361d62ea6bd47af7fbf20863
Tarun Boddeda <121426564+TarunB-Git@users.noreply.github.com>
2025-07-14 04:19:27 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,11 +1,11 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "sqlite:///travel_budget.db"
-    )
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "..", "instance", "travel_budget.db")
+    SQLALCHEMY_BINDS = {
+        "messages": "sqlite:///" + os.path.join(basedir, "..", "instance", "messages.db")
+    }
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY", "change-me")
-    SQLALCHEMY_BINDS = {
-    "messages": "sqlite:///instance/messages.db"
-}
