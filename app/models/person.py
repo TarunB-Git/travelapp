f84189c02257707cb7307beac90b6425c561714f
Tarun Boddeda <121426564+TarunB-Git@users.noreply.github.com>
2025-07-13 23:47:10 +0700

    Update person.py

--- app/models/person.py
+++ app/models/person.py
@@ -3,5 +3,6 @@ from app.extensions import db
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), unique=True, nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey("group.id"))
 
 
