3caf8bfb275dd872c50f86a8cd05431527d5be4a
Tarun Boddeda <121426564+TarunB-Git@users.noreply.github.com>
2025-07-14 02:39:06 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .config import Config
-from .extensions import db
+from .extensions import db, messages_db
 from .routes import register_blueprints
 
 def create_app() -> Flask:
@@ -8,7 +8,9 @@ def create_app() -> Flask:
     app.config.from_object(Config)
 
     db.init_app(app)
+    messages_db.init_app(app)
     register_blueprints(app)
 
     return app
+
 
